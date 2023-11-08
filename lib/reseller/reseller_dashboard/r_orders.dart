eb24f13dca386b7958d520cb3aa4dc210eedaa22
Njoki <njokicynthia590@gmail.com>
2023-11-08 11:45:56 +0300

    Add reseller orders status color codes

--- lib/reseller/reseller_dashboard/r_orders.dart
+++ lib/reseller/reseller_dashboard/r_orders.dart
@@ -100,17 +100,17 @@ class _ResellerOrdersState extends State<ResellerOrders> {
   Color getStatusColor(int status) {
     switch (status) {
       case 1:
-        return Colors.blue;
+        return const Color.fromARGB(255, 35, 31, 31);
       case 2:
-        return Colors.greenAccent;
-      case 3:
-        return Colors.black;
-      case 4:
         return Colors.orange;
+      case 3:
+        return Colors.purple;
+      case 4:
+        return Color.fromRGBO(27, 197, 189, 1.0);
       case 5:
         return Colors.red;
       default:
-        return Colors.black; // You can specify the default color here
+        return Colors.black;
     }
   }
 
@@ -118,16 +118,14 @@ class _ResellerOrdersState extends State<ResellerOrders> {
     switch (status) {
       case 1:
         return const Color.fromARGB(
-            255, 187, 222, 251); // Lighter shade of blue
+            255, 245, 245, 245); // Lighter shade of blue
       case 2:
-        return Color.fromARGB(255, 225, 225, 225);
+        return const Color.fromARGB(255, 255, 224, 178);
       //const Color.fromARGB(255, 255, 249, 196);
       case 3:
-        return const Color.fromARGB(
-            255, 245, 245, 245); // Lighter shade of grey
+        return Color.fromARGB(255, 252, 237, 252); // Lighter shade of grey
       case 4:
-        return const Color.fromARGB(
-            255, 255, 224, 178); // Lighter shade of orange
+        return Color.fromRGBO(201, 247, 245, 1.0); // Lighter shade of orange
       case 5:
         return const Color.fromARGB(255, 255, 205, 210); // Lighter shade of red
       default:
@@ -274,14 +272,6 @@ class _ResellerOrdersState extends State<ResellerOrders> {
                                                   fontSize: 16,
                                                 ),
                                               ),
-                                              // child: Text(
-                                              //   order.orderStatus,
-                                              //   style: TextStyle(
-                                              //     color: Color.fromRGBO(
-                                              //         246, 78, 96, 1.0),
-                                              //     fontSize: 12,
-                                              //   ),
-                                              // ),
                                             ),
                                           ),
                                           SizedBox(
@@ -295,27 +285,32 @@ class _ResellerOrdersState extends State<ResellerOrders> {
                                     mainAxisAlignment:
                                         MainAxisAlignment.spaceBetween,
                                     children: [
-                                      RichText(
-                                        text: TextSpan(
-                                          style: DefaultTextStyle.of(context)
-                                              .style,
-                                          children: <TextSpan>[
-                                            TextSpan(
-                                              text:
-                                                  "KES ${order.orderPayableAmount}/",
-                                              style: boldText,
-                                            ),
-                                            TextSpan(
-                                              text:
-                                                  '${order.orderVolume ?? ''}',
-                                              style: TextStyle(
-                                                fontSize: 14,
-                                                color: Colors.grey,
-                                              ), // Style for the unit
-                                            ),
-                                          ],
-                                        ),
+                                      Text(
+                                        'KES ${order.orderPayableAmount}',
+                                        style: boldText.copyWith(
+                                            color: primaryDarkColor),
                                       ),
+                                      // RichText(
+                                      //   text: TextSpan(
+                                      //     style: DefaultTextStyle.of(context)
+                                      //         .style,
+                                      //     children: <TextSpan>[
+                                      //       TextSpan(
+                                      //         text:
+                                      //             "KES ${order.orderPayableAmount}/",
+                                      //         style: boldText,
+                                      //       ),
+                                      //       TextSpan(
+                                      //         text:
+                                      //             '${order.orderVolume ?? ''}',
+                                      //         style: TextStyle(
+                                      //           fontSize: 14,
+                                      //           color: Colors.grey,
+                                      //         ), // Style for the unit
+                                      //       ),
+                                      //     ],
+                                      //   ),
+                                      // ),
                                       Text(
                                         "View more",
                                         style: TextStyle(
